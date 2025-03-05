af106f6ebb3c6f413f3f0098521949806b73e402
Paweł Radej <pawel.radej@outlook.com>
2025-03-05 07:36:13 +0100

    Fixed iterators in SxArrayProperty and SxDictionaryProperty

--- addons/signal_extensions/properties/sx_array_property.gd
+++ addons/signal_extensions/properties/sx_array_property.gd
@@ -16,66 +16,65 @@ signal value_changed(type, array, payload)
 
 var value: Array = []:
 	get: return value
-	
+
 var _last_size := 0
-var _iter_current_index := 0
-	
+
 
 func _init(initialvalue: Array = []):
 	value = initialvalue
 	_last_size = value.size()
-	
+
 	Sx.from(value_changed).filter(func(type: SxArrayProperty.Event, array: Array, _z):
 		return type != Event.COUNT_CHANGED and _last_size != array.size()
-	).subscribe(func(_x, array: Array, _z): 
+	).subscribe(func(_x, array: Array, _z):
 		value_changed.emit(Event.COUNT_CHANGED, array, array.size())
 		_last_size = array.size()
 	)
 
 
-func _iter_init(_arg) -> bool:
-	_iter_current_index = 0
-	return _iter_should_continue()
+func _iter_init(arg) -> bool:
+	arg[0] = 0
+	return _iter_should_continue(arg)
 
 
-func _iter_next(_arg) -> bool:
-	_iter_current_index += 1
-	return _iter_should_continue()
+func _iter_next(arg) -> bool:
+	arg[0] += 1
+	return _iter_should_continue(arg)
 
 
-func _iter_get(_arg) -> Variant:
-	return value[_iter_current_index]
+func _iter_get(current_index) -> Variant:
+	return value[current_index]
 
 
-func _iter_should_continue() -> bool:
-	return _iter_current_index < size()
+func _iter_should_continue(arg) -> bool:
+	return arg[0] < size()
 
-	
+
 func append(item: Variant) -> void:
 	value.append(item)
 	value_changed.emit(Event.UPDATED, value, item)
-	
-	
+
+
 func append_array(items: Array) -> void:
 	value.append_array(items)
 	value_changed.emit(Event.UPDATED_LIST, value, items)
-	
+
 
 func assign(items: Array) -> void:
 	value.assign(items)
 	value_changed.emit(Event.UPDATED_LIST, value, items)
-	
-	
+
+
 func clear() -> void:
 	value.clear()
 	value_changed.emit(Event.CLEARED, value, null)
-	
-	
+
+
 func erase(item: Variant) -> void:
 	value.erase(item)
 	value_changed.emit(Event.ERASED, value, item)
-	
-	
+
+
 func fill(item: Variant) -> void:
 	value.fill(item)
 	value_changed.emit(Event.UPDATED_LIST, value, value)
@@ -88,64 +87,64 @@ func get_index(position: int) -> Variant:
 func has(item: Variant) -> bool:
 	return value.has(item)
 
-	
+
 func insert(position: int, item: Variant) -> void:
 	value.insert(position, item)
 	value_changed.emit(Event.UPDATED, value, item)
-	
-	
+
+
 func pop_at(index: int) -> Variant:
 	var item = value.pop_at(index)
 	value_changed.emit(Event.ERASED, value, item)
 	return item
-	
-	
+
+
 func pop_back() -> Variant:
 	var item = value.pop_back()
 	value_changed.emit(Event.ERASED, value, item)
 	return item
-	
-	
+
+
 func pop_front() -> Variant:
 	var item = value.pop_front()
 	value_changed.emit(Event.ERASED, value, item)
 	return item
-	
-	
+
+
 func push_back(item: Variant) -> void:
 	value.push_back(item)
 	value_changed.emit(Event.UPDATED, value, item)
-	
-	
+
+
 func push_front(item: Variant) -> void:
 	value.push_back(item)
 	value_changed.emit(Event.UPDATED, value, item)
-	
-	
+
+
 func remove_at(position: int) -> void:
 	var item = value[position]
 	value.remove_at(position)
 	value_changed.emit(Event.ERASED, value, item)
-	
-	
+
+
 func set_index(position: int, item: Variant) -> void:
 	value[position] = item
 	value_changed.emit(Event.UPDATED, value, item)
-	
-	
+
+
 func size() -> int:
 	return value.size()
 
 
 ## Observes every type of event with [enum Event] in the [SxArrayProperty].
 ## [b]emit_initialvalue[/b] can be set to false to not emit current value on subscription.
-## Returns [SxSignal](Event, array, payload).	
+## Returns [SxSignal](Event, array, payload).
 func as_signal(emit_initialvalue := true) -> SxSignal:
 	var result := Sx.from(value_changed)
 	if emit_initialvalue:
 		result = result.start_with(func(): return [Event.UPDATED_LIST, value, value])
 	return result
-	
+
 
 ## Observes specific event with [enum Event]
 ## [b]emit_initialvalue[/b] can be set to false to not emit current value on subscription.
--- addons/signal_extensions/properties/sx_dictionary_property.gd
+++ addons/signal_extensions/properties/sx_dictionary_property.gd
@@ -16,48 +16,47 @@ signal value_changed(type, array, payload)
 
 var value: Dictionary = {}:
 	get: return value
-	
+
 var _last_size := 0
-var _iter_current_index := 0
 var _iter_keys := []
-	
+
 
 func _init(initial_value: Dictionary = {}):
 	value = initial_value
 	_last_size = value.size()
-	
+
 	Sx.from(value_changed).filter(func(type: SxDictionaryProperty.Event, dict: Dictionary, _z):
 		return type != Event.COUNT_CHANGED and _last_size != dict.size()
-	).subscribe(func(_x, dict: Dictionary, _z): 
+	).subscribe(func(_x, dict: Dictionary, _z):
 		value_changed.emit(Event.COUNT_CHANGED, dict, dict.size())
 		_last_size = dict.size()
 	)
 
 
-func _iter_init(_arg) -> bool:
+func _iter_init(arg) -> bool:
 	_iter_keys = keys()
-	_iter_current_index = 0
-	return _iter_should_continue()
+	arg[0] = 0
+	return _iter_should_continue(arg)
 
 
-func _iter_next(_arg) -> bool:
-	_iter_current_index += 1
-	return _iter_should_continue()
+func _iter_next(arg) -> bool:
+	arg[0] += 1
+	return _iter_should_continue(arg)
 
 
-func _iter_get(_arg) -> Variant:
-	return value[_iter_keys[_iter_current_index]]
+func _iter_get(current_index) -> Variant:
+	return value[_iter_keys[current_index]]
 
 
-func _iter_should_continue() -> bool:
-	return _iter_current_index < size()
+func _iter_should_continue(arg) -> bool:
+	return arg[0] < size()
 
-	
+
 func clear() -> void:
 	value.clear()
 	value_changed.emit(Event.CLEARED, value, null)
-	
-	
+
+
 func erase(item: Variant) -> void:
 	value.erase(item)
 	value_changed.emit(Event.ERASED, value, item)
@@ -67,34 +66,34 @@ func get_value(key: Variant, default: Variant = null) -> Variant:
 	if not has(key):
 		return default
 	return value[key]
-	
-	
+
+
 func has(item: Variant) -> bool:
 	return value.has(item)
-	
-	
+
+
 func keys() -> Array:
 	return value.keys()
-	
-	
+
+
 func merge(dictionary: Dictionary, overwrite: bool = false) -> void:
 	value.merge(dictionary, overwrite)
 	value_changed.emit(Event.UPDATED_LIST, value, dictionary)
-	
-	
-	
+
+
+
 func set_value(key: Variant, item: Variant) -> void:
 	value[key] = item
 	value_changed.emit(Event.UPDATED, value, key)
-	
-	
+
+
 func size() -> int:
 	return value.size()
-	
-	
+
+
 func values() -> Array:
 	return value.values()
-	
+
 
 ## Observes every type of event with [enum Event] in the [SxDictionaryProperty].
 ## [b]emit_initial_value[/b] can be set to false to not emit current value on subscription.
@@ -104,7 +103,7 @@ func as_signal(emit_initial_value := true) -> SxSignal:
 	if emit_initial_value:
 		result = result.start_with(func(): return [Event.UPDATED_LIST, value, value])
 	return result
-	
+
 
 ## Observes specific event with [enum Event]
 ## Returns [SxSignal](dictionary, payload).
